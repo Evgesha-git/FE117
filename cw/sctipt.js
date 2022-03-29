e324bfa119a3690b6bf7dec360555101501b75f0
FE117 <chico7496@gmail.com>
2022-03-29 21:45:41 +0300

    cw 29.03 + hw

--- cw/sctipt.js
+++ cw/sctipt.js
@@ -1,145 +1,45 @@
-// let a = 1;
-// const b = 2;
-// var c = 3;
+let days = 250;
 
-// // b = 3;
+let year = parseInt(days / 365);
+let mount = parseInt(days / 31);
+let week = parseInt(days / 7);
+let hour = days * 24;
+let min = hour * 60;
+let sec = min * 60;
 
-// const obj = {
-//     a: 1,
-//     b: 2
-// }
+year < 1 ? console.log('Меньше года') : console.log(year);
+mount < 1 ? console.log('Меньше месяца') : console.log(mount);
 
-// console.table(obj);
 
-// obj.a = 5;
+console.log(week);
 
-// console.dir(obj);
+let realMount = 0;
 
-// {
-//     let a = 4;
-//     var c = 10;
-//     // console.log(a);
-//     console.warn(c);
-// }
-
-// // console.log(a);
-// console.error(c);
-
-// var c = 15;
-// console.log(c);
-
-// console.log('2' + 2);
-
-// let a = '1001011';
-
-// console.log(Number(a));
-
-// console.log(parseInt(a, 2));
-
-// console.log(parseFloat(a));
-
-// console.log(+a);
-
-// let b = 11;
-
-// console.log(String(b));
-
-// console.log(b.toString());
-
-// console.log(b + '');
-
-let a1 = 5 % 3,
-    a2 = 3 % 5,
-    a3 = 5 + '3',
-    a4 = '5' - 3,
-    a5 = 75 + 'кг',
-    a6 = 785 * 653,
-    a7 = 100 / 25,
-    a16 = 1 || 0,
-    a17 = false || true,
-    a18 = true > 0;
-
-console.log(`
-a1 = ${a1},
-a2 = ${a2},
-a3 = ${a3},
-a4 = ${a4},
-a5 = ${a5},
-a6 = ${a6},
-a7 = ${a7},
-a16 = ${a16},
-a17 = ${a17},
-a18 = ${a18},
-`)
-
-// 3
-
-let width = '23см';
-let height = '10см';
-let SPryam = parseInt(width) * parseInt(height);
-
-console.log(SPryam);
-
-// 4
-
-let h = '10m';
-let VCilindra = parseInt(h) * a7;
-
-console.log(VCilindra);
-
-// 6
-
-{
-    let a = '5sm',
-        b = '7sm',
-        h = '10sm',
-        STrap = ((parseInt(a) + parseInt(b)) / 2) * parseInt(h);
-    
-    let aN = a.split(/\D/)[0];
-    let aS = a.split(/\d/).reverse()[0];
-    console.table([aN, aS]);
-    console.log(STrap);
+if (mount > 12){
+    if (year > 1)realMount = mount - (12 * (year - 1));
+    else realMount = mount - 12;
+}else{
+    realMount = mount;
 }
 
-// 7
-
-{
-    let s = 2000000;
-    let p = 10;
-    let years = 5;
-    let rez = (s * (p / 100)) * years;
-    console.log(rez);
-}
-
-// 9 
-
-console.log(`
-Бывало, спит у ног собака,
-костер занявшийся гудит,
-и женщина из полумрака
-глазами зыбкими глядит.
-
-Потом под пихтою приляжет
-на куртку рыжую мою
-и мне, задумчивая, скажет:
-
-"А ну-ка, спой!.."- и я пою. 
-`);
-
-//('' undefined NaN null 0 false) = false
-
-let x = 10;
-
-if (x) console.log('правда');
-else console.log('неправда');
-
-x > 9 ? console.log('правда') : console.log('неправда');
-
-function f(x){
-    return x > 9 ? 'правда' : null;
-    // if (x > 9){
-    //     return 'правда';
-    // }else{
-    //     return 'неправда';
-    // }
+// 10 задание
+console.log(realMount)
+switch (realMount){
+    case 12:
+    case 1:
+    case 2:
+        console.log('Зима')
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна')
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Лето')
+        break;
+    default:
+        console.log('Осень')
 }
